663de25e498758e535f1f49f562ad628ad06d427
HCE <cavell1blackburn@gmail.com>
2025-10-03 19:01:56 +0100

    Made the deck and card objects

---
+++ app/src/main/java/org/example/Card.java
@@ -0,0 +1,34 @@
+package org.example;
+
+public class Card {
+    enum Suit {
+        HEART,
+        CLUB,
+        SPADE,
+        DIAMOND
+    }
+
+    enum Rank {
+        TWO,
+        THREE,
+        FOUR,
+        FIVE,
+        SIX,
+        SEVEN,
+        EIGHT,
+        NINE,
+        TEN,
+        JACK,
+        QUEEN,
+        KING,
+        ACE
+    }
+
+    Suit suit;
+    Rank rank;
+
+    public Card(Suit suit, Rank rank) {
+        this.suit = suit;
+        this.rank = rank;
+    }
+}
---
+++ app/src/main/java/org/example/Deck.java
@@ -0,0 +1,20 @@
+package org.example;
+import java.util.LinkedList;
+import org.example.Card;
+import org.example.Card.Suit;
+import org.example.Card.Rank;
+
+public class Deck {
+    LinkedList<Card> deck;
+    int cardInDeck;
+
+    public Deck() {
+        this.deck = new LinkedList<>();
+        for (Suit suit : Suit.values()) {
+            for (Rank rank : Rank.values()) {
+                deck.add(new Card(suit, rank));
+                cardInDeck++;
+            }
+        }
+    }
+}
